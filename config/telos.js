0eebb6139db6bebdc482e67b1ab39ea42f468e40
simon <simon@klein-homepage.de>
2023-10-30 21:52:45 +0300

    changed tlos stable pairs token0<->token1 to fix price calculations

--- config/telos.js
+++ config/telos.js
@@ -7,8 +7,8 @@ module.exports = {
   v2: {
     factoryAddress: "0x9e6d21e759a7a288b80eef94e4737d313d31c13f",
     startBlock: 286182911,
-    wNativeStablePair0: "0x0000000000000000000000000000000000000000",
-    wNativeStablePair1: "0x86ca8345bda0d6052e93f07be4dcc680af927d53", // WTLOS/USDTi
+    wNativeStablePair0: "0x86ca8345bda0d6052e93f07be4dcc680af927d53", // USDTi/WTLOS
+    wNativeStablePair1: "0x0000000000000000000000000000000000000000",
     whitelistAddresses: [
       "0xdc2393dc10734bf153153038943a5deb42b209cd", // WTLOS
       "0xb25cb6a275a8d6a613228fb161eb3627b50eb696", // ICE
