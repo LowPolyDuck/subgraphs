99e99efeb04ceb81ccc112a2a9111231822e5d2a
simon <simon@klein-homepage.de>
2023-11-02 13:38:20 +0300

    made Core constants addresses properly lowercase

--- config/core.js
+++ config/core.js
@@ -6,9 +6,9 @@ module.exports = {
   wNativeAddress: "0x9550db7d4d7fb115bd3c5cdef861e7d7d2f5062a", // for V2 subgraph, use old WCORE: 0x40375c92d9faf44d2f9db9bd9ba41a3317a2404f, rest 0x9550db7d4d7fb115bd3c5cdef861e7d7d2f5062a
   v3: {
     // WCORE-USDT 0.3%
-    wNativeStablePoolAddress: "0x761900d52C13c0ec76B2FB117fe3D3674339fE15",
+    wNativeStablePoolAddress: "0x761900d52c13c0ec76b2fb117fe3d3674339fe15",
     stableIsToken0: true,
-    factoryAddress: "0x0DC54Cd1356E3ca8e876A4D44512325D8B2DC589",
+    factoryAddress: "0x0dc54cd1356e3ca8e876a4d44512325d8b2dc589",
     startBlock: 7942233,
     stableCoins: [
       "0x81bcea03678d1cef4830942227720d542aa15817", // USDT ICE Bridge
@@ -24,12 +24,12 @@ module.exports = {
       "0xd2683b22287e63d22928cbe4514003a92507f474", // USDC ICE Bridge
       "0xa4151b2b3e269645181dccf2d426ce75fcbdeca9", // USDC L0 Bridge
     ],
-    nonfungiblePositionManagerAddress: "0xfb7bcf090937F870e6E72f9a31e0839aa2a7b513",
+    nonfungiblePositionManagerAddress: "0xfb7bcf090937f870e6e72f9a31e0839aa2a7b513",
     nonfungiblePositionManagerStartBlock: 7942239,
     minETHLocked: 1,
   },
   masterChefV3: {
-    masterChefAddress: "0xBf66585582e5aD5353D9067dF3E9394C305bd731",
+    masterChefAddress: "0xbf66585582e5ad5353d9067df3e9394c305bd731",
     startBlock: 7942243,
   },
   v2: {
