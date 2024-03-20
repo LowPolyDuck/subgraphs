bcd428cd9bc5658e09a20318fd4e2f6e37e3f6a6
Oscar Guertler <oscarguertler@skiff.com>
2024-03-20 18:10:03 +0100

    chore: update conf

--- config/shardeumTestnet.js
+++ config/shardeumTestnet.js
@@ -3,17 +3,13 @@
  */
 module.exports = {
   network: "shardeumTestnet",
-  wNativeAddress: "0x5eB65C6feC23d4eb36bC9966aA110Fe13FBd7c7F".toLowerCase(),
+  wNativeAddress: "0x9e6d21e759a7a288b80eef94e4737d313d31c13f".toLowerCase(),
   v2: {
-    factoryAddress: "0x9E6d21E759A7A288b80eef94E4737D313D31c13f".toLowerCase(),
-    startBlock: 2100,
-    wNativeStablePair0: "0x2a91593ec8bafc9cf06bb8584b6e76ddc3fa8381".toLowerCase(), // WSHM/USDT
+    factoryAddress: "0x3D4440F335060a0341C9E6C3bBeE85E552505FFF".toLowerCase(),
+    startBlock: 111331,
+    wNativeStablePair0: "0x0000000000000000000000000000000000000000".toLowerCase(), // WSHM/USDT
     wNativeStablePair1: "0x0000000000000000000000000000000000000000".toLowerCase(),
-    whitelistAddresses: [
-      "0x5eB65C6feC23d4eb36bC9966aA110Fe13FBd7c7F".toLowerCase(), // WSHM
-      "0xb5C7882e37359572FD1cCFAA276e7Fdf70145D42".toLowerCase(), // ICE
-      "0x43891084581fD07Ee1189f3a2f04e51c26a95B77".toLowerCase(), // USDTi
-    ],
+    whitelistAddresses: [],
     minNativeForPricing: "0.1",
   },
 };
